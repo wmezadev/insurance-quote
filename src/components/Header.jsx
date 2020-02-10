18f7d57e1764b700ec9dbe9fed3ca67a1294aa48
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-10 14:33:53 -0500

    Feat - Create styled component for App Title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 
+const HeaderContainer = styled.header`
+  background-color: #26C6DA;
+  padding: 10px;
+  font-weight: bold;
+  color: #FFFFFF;
+`;
+
+const HeaderText = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`
 
 const Header = ({ title }) => {
     return ( 
-        <h1>
-            { title }
-        </h1> 
+        <HeaderContainer>
+            <HeaderText>{ title }</HeaderText>
+        </HeaderContainer> 
     );
 }
  
