28cf4fdc2838ada4229c30fb5ca2ad0605d67a78
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-10 14:36:52 -0500

    Feat - Create child styled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header 
-      title="Insurance Quote"
-    />
+    <Container>
+      <Header 
+        title="Insurance Quote"
+      />
+      <FormContainer>
+        
+      </FormContainer>
+    </Container>
   );
 }
 
